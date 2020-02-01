227b129667dc94c5c8967e8f586ddea991726014
M3RLN <32886755+M3RLN@users.noreply.github.com>
2020-02-01 11:20:59 -0800

    Started Coding the Script

--- Assets/Scripts/ListScript.cs
+++ Assets/Scripts/ListScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+    public List<GameObject> Tilemap2DArray;
     // Start is called before the first frame update
     void Start()
     {
