f82bfea406a082c6807eea33a1d305207e2c769f
syy1125 <syy1125@hotmail.com>
2020-02-01 12:47:01 -0800

    Start work on building refactor

---
+++ Assets/ScriptableObjects/BuildingData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+// A BuildingData object represents a certain kind of building
+// e.g. A library, or a house
+[CreateAssetMenu(menuName="Scriptable Objects/Building Data")]
+public class BuildingData : ScriptableObject
+{
+	public Tilemap BuildingShape;
+}
---
+++ Assets/ScriptableObjects/BuildingSpace.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// A building space represents a certain size of ruins that you can replace with a building
+// e.g. A 2x2 ruin that can be replaced by either a house or a library
+[CreateAssetMenu(menuName="Scriptable Objects/Building Space")]
+public class BuildingSpace : ScriptableObject
+{
+	/// <summary>
+	/// Should contain a tilemap
+	/// </summary>
+	public GameObject RuinShape;
+	public BuildingData[] RepairOptions;
+}
