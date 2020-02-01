e187d88b00cdd6ae77c79a2229ae3769c142df34
syy1125 <syy1125@hotmail.com>
2020-02-01 15:33:11 -0800

    Start work on building logic components

---
+++ Assets/Scripts/Buildings/BuildingHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BuildingInfo))]
+public class BuildingHealth : MonoBehaviour
+{
+	public int MaxHealth { get; private set; }
+	public int CurrentHealth { get; private set; }
+
+	private void Start()
+	{
+		BuildingInfo info = GetComponent<BuildingInfo>();
+
+		MaxHealth = info.BaseMaxHealth; // TODO query GameManager for health multiplier
+		CurrentHealth = MaxHealth;
+	}
+
+	public void DealDamage(int amount)
+	{
+		CurrentHealth -= amount;
+		if (CurrentHealth <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Buildings/BuildingInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public enum BuildingType
+{
+	Mind,
+	Body,
+	Soul
+}
+
+public class BuildingInfo : MonoBehaviour
+{
+	public BuildingType BuildingType;
+	public int BaseCost;
+	public int BaseMaxHealth;
+}
