85c3c39458e2aa89629b05282e5a04a902e4aca2
gavindent <gavindent@hotmail.com>
2020-02-01 11:01:50 -0800

    SceneManagement prefab created. SceneSChanger script added.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string mainGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene(mainGame);
+    }
+
+    public void LoadTitleScreen()
+    {
+
+    }
+
+    public void LoadEndGame()
+    {
+
+    }
+}
