616b3210d8d53dd8cc5c201fb066609a40e62a35
syy1125 <syy1125@hotmail.com>
2020-01-31 22:13:04 -0800

    Create color inversion shader

---
+++ Assets/Scripts/CameraShader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraShader : MonoBehaviour
+{
+	public Shader Shader;
+	private Material _material;
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if (Shader == null) return;
+		if (_material == null || _material.shader != Shader)
+		{
+			_material = new Material(Shader);
+		}
+
+		Graphics.Blit(src, dest, _material);
+	}
+}
