851f868e026f9dc2f11f1ef97be94ae7cd0c7b6c
syy1125 <syy1125@hotmail.com>
2020-02-02 14:13:38 -0800

    Implement random map environent generation

---
+++ Assets/Scripts/MapRandomizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[Serializable]
+public struct TileOption
+{
+	public TileBase Tile;
+	public float Weight;
+}
+
+public class MapRandomizer : MonoBehaviour
+{
+	public Tilemap Map;
+	public TileOption[] OptionsPool;
+
+	public void RandomizeTiles()
+	{
+		BuildingManager buildingManager = GetComponent<BuildingManager>();
+
+		foreach (Vector3Int position in Map.cellBounds.allPositionsWithin)
+		{
+			if (Map.HasTile(position) && !buildingManager.HasRuin(position) && !buildingManager.HasActiveBuilding(position))
+			{
+				Map.SetTile(position, ChooseTile());
+			}
+		}
+	}
+
+	private TileBase ChooseTile()
+	{
+		float totalWeight = OptionsPool.Sum(option => option.Weight);
+		float chosenWeight = totalWeight * UnityEngine.Random.value;
+		foreach (TileOption option in OptionsPool)
+		{
+			chosenWeight -= option.Weight;
+			if (chosenWeight < 0)
+			{
+				return option.Tile;
+			}
+		}
+		Debug.LogWarning("Unexpected branch in ChooseTile");
+		Debug.LogWarning($"Total weight {totalWeight}");
+		return OptionsPool[OptionsPool.Length - 1].Tile;
+	}
+}
