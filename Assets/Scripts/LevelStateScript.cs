f960c225e1d7ca7a4b42db27c03e54aad986dbdc
Brian Leip <brianleip@gmail.com>
2020-02-01 21:44:15 -0800

    got rid of Debug.Log spams on LevelStateScript

--- Assets/Scripts/LevelStateScript.cs
+++ Assets/Scripts/LevelStateScript.cs
@@ -55,11 +55,8 @@ public class LevelStateScript : MonoBehaviour
             BuildingHealth bh = bldg.GetComponent<BuildingHealth>();
             //BuildingInfo bi = bldg.GetComponent<BuildingInfo>();
             totalBldgHealth += bh.CurrentHealth;
-            Debug.Log("Building health increment: " + bh.CurrentHealth);
-
             //buildingCost += bi.BaseCost;
         }
-        Debug.Log("Total Building Health: " + totalBldgHealth);
 
         pss.SetBody(totalBldgHealth);
         //pss.SetMind(pss.GetMind() - buildingCost);
